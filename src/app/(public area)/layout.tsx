aa9c0016b9ceb61b76ea3fa04efa0cfd8d123731
Dusan <dusanbojanic1@gmail.com>
2025-04-10 13:36:56 +0200

    removed whitespace

--- src/app/(public area)/layout.tsx
+++ src/app/(public area)/layout.tsx
@@ -21,14 +21,18 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-        <body className={inter.className}>
-          <div className="relative min-h-screen w-full bg-gradient-to-br from-blue-900 via-purple-900 to-red-900">
+        <body className={`${inter.className}`}>
+          <div className="relative min-h-screen min-w-max bg-gradient-to-br from-blue-900 via-purple-900 to-red-900">
             <AnimatedCelestialBodies />
             <HamburgerMenu />
 
-            <main className="relative z-40 min-h-[80vh] w-full px-4">
-              <TRPCReactProvider>{children}</TRPCReactProvider>
-            </main>
+            <div className="flex flex-col items-center">
+              <main className="relative z-40 flex min-h-[80vh] w-full flex-col items-center justify-center px-4 sm:min-h-[60vh] sm:px-6 md:px-8">
+                <div className="w-full max-w-screen-lg">
+                  <TRPCReactProvider>{children}</TRPCReactProvider>
+                </div>
+              </main>
+            </div>
           </div>
         </body>
       </AuthProvider>
