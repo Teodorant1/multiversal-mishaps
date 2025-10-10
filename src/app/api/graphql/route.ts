0aeac487fa571f466be0de96b824a1986d07c872
Dusan <dusanbojanic1@gmail.com>
2025-10-10 10:00:18 +0200

    commented out the graphql page because it was causing errors

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,12 +1,12 @@
-import { createYoga, createSchema } from "graphql-yoga";
-import { type NextRequest } from "next/server";
-import { schema } from "./schema";
-export const { handleRequest } = createYoga<{
-  req: NextRequest;
-}>({
-  schema,
-  graphqlEndpoint: "/api/graphql",
-  fetchAPI: { Request: Request, Response: Response },
-});
+// import { createYoga, createSchema } from "graphql-yoga";
+// import { type NextRequest } from "next/server";
+// import { schema } from "./schema";
+// export const { handleRequest } = createYoga<{
+//   req: NextRequest;
+// }>({
+//   schema,
+//   graphqlEndpoint: "/api/graphql",
+//   fetchAPI: { Request: Request, Response: Response },
+// });
 
-export { handleRequest as GET, handleRequest as POST };
+// export { handleRequest as GET, handleRequest as POST };
