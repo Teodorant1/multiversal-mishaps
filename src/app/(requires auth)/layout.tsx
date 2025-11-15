7c25cc8d47e06c29bae7a284dd4e019dba41c4c1
Dusan <dusanbojanic1@gmail.com>
2025-11-15 01:23:17 +0100

    fixed layout

--- src/app/(requires auth)/layout.tsx
+++ src/app/(requires auth)/layout.tsx
@@ -3,12 +3,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "~/styles/globals.css";
 import { AnimatedCelestialBodies } from "~/components/AnimatedCelestialBodies";
-import { TRPCReactProvider } from "~/trpc/react";
-import AuthProvider from "../_components/auth/Provider";
 import { auth } from "~/server/auth";
 import { HamburgerMenu } from "~/components/HamburgerMenu";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Multiversal Mishaps",
@@ -24,32 +20,26 @@ export default async function RootLayout({
   const session = await auth();
 
   return (
-    <html lang="en">
-      <AuthProvider>
-        <body className={inter.className}>
-          <div className="relative min-h-screen w-full bg-gradient-to-br from-blue-900 via-purple-900 to-red-900">
-            {session && (
-              <>
-                <AnimatedCelestialBodies />
-                <HamburgerMenu />
-              </>
-            )}
-            <div className="flex flex-col items-center">
-              <div className="relative z-40 flex min-h-[80vh] w-full flex-col items-center justify-center px-4 sm:min-h-[60vh] sm:px-6 md:px-8">
-                {session ? (
-                  <div className="w-full max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg">
-                    <TRPCReactProvider>{children}</TRPCReactProvider>
-                  </div>
-                ) : (
-                  <div className="w-full max-w-screen-sm px-4 text-center">
-                    <AuthRequired />
-                  </div>
-                )}
-              </div>
+    <div className="relative min-h-screen w-full bg-gradient-to-br from-blue-900 via-purple-900 to-red-900">
+      {session && (
+        <>
+          <AnimatedCelestialBodies />
+          <HamburgerMenu />
+        </>
+      )}
+      <div className="flex flex-col items-center">
+        <div className="relative z-40 flex min-h-[80vh] w-full flex-col items-center justify-center px-4 sm:min-h-[60vh] sm:px-6 md:px-8">
+          {session ? (
+            <div className="w-full max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg">
+              {children}
             </div>
-          </div>
-        </body>
-      </AuthProvider>
-    </html>
+          ) : (
+            <div className="w-full max-w-screen-sm px-4 text-center">
+              <AuthRequired />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
