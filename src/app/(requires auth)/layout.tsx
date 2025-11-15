9e1f5bcbc3baee1fb8df00e00caa626bb2ff2c62
Dusan <dusanbojanic1@gmail.com>
2025-11-15 01:51:34 +0100

    decluttered the authrequired layout and fixed the double gradient bug

--- src/app/(requires auth)/layout.tsx
+++ src/app/(requires auth)/layout.tsx
@@ -17,20 +17,16 @@ export default async function RootLayout({
   const session = await auth();
 
   return (
-    <div className="relative min-h-screen w-full bg-gradient-to-br from-blue-900 via-purple-900 to-red-900">
-      <div className="flex flex-col items-center">
-        <div className="relative z-40 flex min-h-[80vh] w-full flex-col items-center justify-center px-4 sm:min-h-[60vh] sm:px-6 md:px-8">
-          {session ? (
-            <div className="w-full max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg">
-              {children}
-            </div>
-          ) : (
-            <div className="w-full max-w-screen-sm px-4 text-center">
-              <AuthRequired />
-            </div>
-          )}
+    <div>
+      {session ? (
+        <div className="w-full max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg">
+          {children}
         </div>
-      </div>
+      ) : (
+        <div className="w-full max-w-screen-sm px-4 text-center">
+          <AuthRequired />
+        </div>
+      )}
     </div>
   );
 }
